e0160b023b60db80143cce54442fee08e00b6e71
aderesh <andryakaad@gmail.com>
2020-12-13 10:55:48 -0500

    initial

---
+++ Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Xml;
+
+namespace namesilo
+{
+    class Program
+    {
+        static string DOMAIN;
+        static string APIKEY;
+        static string HOST;
+
+        static string GetMyApi()
+        {
+            var client = new HttpClient();
+            var response = client.GetAsync("https://myexternalip.com/raw").Result;
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        public class Record
+        {
+            public string Id { get; set; }
+            public string IP { get; set; }
+        }
+
+        static Record GetCurrentRecord()
+        {
+            var client = new HttpClient();
+            var response = client.GetAsync($"https://www.namesilo.com/api/dnsListRecords?version=1&type=xml&key={APIKEY}&domain={DOMAIN}").Result;
+            var content = response.Content.ReadAsStringAsync().Result;
+
+            var reply = new XmlDocument();
+            reply.LoadXml(content);
+            var status = reply.SelectSingleNode("/namesilo/reply/code/text()");
+            if (status == null)
+            {
+                return null;
+            }
+
+            if (status.Value != "300")
+            {
+                return null;
+            }
+
+            var key = string.IsNullOrWhiteSpace(HOST) ? DOMAIN : $"{HOST}.{DOMAIN}";
+            var record = reply.SelectSingleNode($"/namesilo/reply/resource_record/host[text()='{key}']");
+
+            if (record == null)
+            {
+                return null;
+            }
+
+            var currentIP = record.ParentNode.SelectSingleNode("value/text()").Value;
+            var id = record.ParentNode.SelectSingleNode("record_id/text()").Value;
+
+            return new Record
+            {
+                Id = id,
+                IP = currentIP
+            };
+        }
+
+        public static void SetCurrentIP(string recordId, string ip)
+        {
+            var client = new HttpClient();
+            var response = client.GetAsync($"https://www.namesilo.com/api/dnsUpdateRecord?version=1&type=xml&key={APIKEY}&domain={DOMAIN}&rrid={recordId}&rrhost={HOST}&rrvalue={ip}&rrttl=3600").Result;
+            var content = response.Content.ReadAsStringAsync().Result;
+
+            var reply = new XmlDocument();
+            reply.LoadXml(content);
+            var status = reply.SelectSingleNode("/namesilo/reply/code/text()");
+            if (status == null)
+            {
+                Console.Error.WriteLine($"Failed to update record: '{recordId}' with IP: '{ip}'.");
+                return;
+            }
+
+            if (status.Value != "300")
+            {
+                Console.Error.WriteLine($"Failed to update record: '{recordId}' with IP: '{ip}'.");
+                return;
+            }
+
+            Console.WriteLine("Updated successfully");
+        }
+
+        static void PrintEnvVariables(TextWriter writer)
+        {
+            writer.WriteLine("ENVIRONMENT VARIABLES: ");
+            var vairables = Environment.GetEnvironmentVariables();
+            foreach (var item in vairables)
+            {
+                var entry = (DictionaryEntry)item;
+                writer.WriteLine($"{entry.Key}={entry.Value}");
+            }
+            writer.WriteLine();
+        }
+
+        static int Main()
+        {
+            Console.WriteLine("Starting");
+
+            const string domainVariableName = "NAMESILO_DOMAIN";
+            const string hostVariableName = "NAMESILO_HOST";
+            const string apiKeyVariableName = "NAMESILO_APIKEY";
+            const string delayKeyVariableName = "NAMESILO_DELAY";
+
+            // DEBUGGING
+            /*
+            Environment.SetEnvironmentVariable(domainVariableName,  "example.org");
+            Environment.SetEnvironmentVariable(hostVariableName,  "subdomain");
+            Environment.SetEnvironmentVariable(apiKeyVariableName,  "<your api key>");
+            */
+            // END
+
+            // PrintEnvVariables(Console.Out);
+
+            DOMAIN = Environment.GetEnvironmentVariable(domainVariableName);
+            HOST = Environment.GetEnvironmentVariable(hostVariableName);
+            APIKEY = Environment.GetEnvironmentVariable(apiKeyVariableName);
+
+            var delayString = Environment.GetEnvironmentVariable(delayKeyVariableName, EnvironmentVariableTarget.Process);
+            var delay = TimeSpan.FromMinutes(5);
+
+            if (string.IsNullOrEmpty(DOMAIN))
+            {
+                Console.Error.WriteLine($"'{domainVariableName}' is not set");
+                return -1;
+            }
+
+            if (string.IsNullOrEmpty(APIKEY))
+            {
+                Console.Error.WriteLine($"'{APIKEY}' is not set");
+                return -1;
+            }
+
+            if (!string.IsNullOrWhiteSpace(delayString))
+            {
+                delay = TimeSpan.Parse(delayString);
+            }
+
+            Console.WriteLine($"{domainVariableName}: {DOMAIN}");
+            Console.WriteLine($"{hostVariableName}: {HOST}");
+            Console.WriteLine($"{apiKeyVariableName}: {APIKEY}");
+            Console.WriteLine($"{delayKeyVariableName}: {delay}");
+
+            while (true)
+            {
+                try
+                {
+                    var expectedIp = GetMyApi();
+                    Console.WriteLine("IP: " + expectedIp);
+
+                    var record = GetCurrentRecord();
+                    Console.WriteLine("Current IP: " + record.IP);
+
+                    if (expectedIp == record.IP)
+                    {
+                        Console.WriteLine("IPs match, skipping");
+                    }
+                    else
+                    {
+                        Console.WriteLine("IPs mismatch, updating");
+                        SetCurrentIP(record.Id, expectedIp);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine(ex);
+                }
+
+                Thread.Sleep(delay);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,85 @@
+## About The Project
+
+DDNS for NameSilo registrar based on .net core 5.0 with docker support.
+Dynamically updates domain record when your dynamic IP is changed. 
+Supported platforms:
+- linux/amd64
+- linux/arm32 (RaspberryPi)
+
+Uses `https://myexternalip.com/` to obtain current IP address.
+
+
+### Docker images
+You can use existing docker images:
+- linux/amd64: aderesh/namesiloddns:latest
+- linux/arm32: aderesh/namesiloddns:latest-arm32v7
+
+### Prerequisites
+#### Obtain NameSilo API key
+API key is needed to update DNS records using NameSilo API. The key should be passed as a `NAMESILO_APIKEY` environment variable.
+You can skip this section if you have existing API key.
+
+To generate new key
+* open https://www.namesilo.com/account/api-manager
+* under 'API Key' section, press 'Generate'
+
+#### To use as a docker image
+Install latest docker
+
+#### To use as a console app
+Install .net core sdk 5.0. 
+
+## Getting Started
+
+You can either use this as a docker container or you can run it as a console app. 
+
+Environment variables:
+* NAMESILO_DOMAIN - (REQUIRED) Domain name. For instance, 'example.org'
+* NAMESILO_HOST - (OPTIONAL) Host name. Default is ''. If your record is 'blog.exmaple.org', your host name is 'blog'. 
+* NAMESILO_APIKEY - (REQUIRED) NameSilo API key. 
+* NAMESILO_DELAY - (OPTIONAL) Requests delay. By default, polls server every 5 minutes.
+
+### To launch using docker
+
+#### Build
+```ps
+docker build -t namesiloddns .
+```
+
+#### Run
+one run
+```
+docker run -d -e "NAMESILO_DOMAIN=example.com" -e "NAMESILO_HOST=blog" -e "NAMESILO_APIKEY=YourNameSiloAPIKey" -e "NAMESILO_DELAY=00:01:00" --name namesiloddns namesiloddns
+```
+
+automatically run on reboot
+```
+ docker run -d -e "NAMESILO_DOMAIN=example.com" -e "NAMESILO_HOST=blog" -e "NAMESILO_APIKEY=YourNameSiloAPIKey" -e "NAMESILO_DELAY=00:01:00" --restart always --name namesiloddns aderesh/namesiloddns:latest-arm32v7
+```
+
+### To launch as a console app
+
+#### Build
+`dotnet restore`
+
+#### Run (Windows)
+```ps
+$env:NAMESILO_DOMAIN="example.com"
+$env:NAMESILO_HOST="blog"
+$env:NAMESILO_APIKEY="YourNameSiloAPIKey"
+$env:NAMESILO_DELAY="00:03:00"
+
+dotnet run
+```
+
+## Contributing
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+## License
+
+Distributed under the MIT License. 
